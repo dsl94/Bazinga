4eae293cb05a36a362e0063dc2056a9df13c9538
DjordjeS <djordjeziga1994@gmail.com>
2017-11-04 23:11:39 +0100

    Location Matcher

---
+++ src/main/java/com/bazinga/Bazinga/match/LocationMatcher.java
@@ -0,0 +1,27 @@
+package com.bazinga.Bazinga.match;
+
+import com.bazinga.Bazinga.model.Offer;
+import com.bazinga.Bazinga.model.User;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by DjordjeZiga on 04/11/2017.
+ */
+@Component
+@Qualifier("locationMatcher")
+public class LocationMatcher implements Matcher {
+    @Override
+    public boolean match(User user, Offer offer) {
+        List<String> offerLocations= Arrays.asList(offer.getLocations().split(","));
+        for (String offerLocation:offerLocations){
+            if (user.getLocations().toLowerCase().contains(offerLocation.toLowerCase())){
+                return true;
+            }
+        }
+        return false;
+    }
+}
