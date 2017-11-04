0e1ae9d1fefee8f0edf006e61eeb4673d53ff72c
nemanja <nemanjabosnjane@gmail.com>
2017-11-04 11:14:52 +0100

    Initial commit

---
+++ src/main/java/com/bazinga/Bazinga/BazingaApplication.java
@@ -0,0 +1,12 @@
+package com.bazinga.Bazinga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BazingaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BazingaApplication.class, args);
+	}
+}
---
+++ src/test/java/com/bazinga/Bazinga/BazingaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.bazinga.Bazinga;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BazingaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
