2209b4a19b28c331d10a36eedb5e099e268ce565
Lazar Petrov <petrovlazar0@gmail.com>
2017-11-04 14:44:41 +0100

    EducationService edit

---
+++ src/main/java/com/bazinga/Bazinga/service/impl/EducationServiceImpl.java
@@ -0,0 +1,44 @@
+package com.bazinga.Bazinga.service.impl;
+
+import com.bazinga.Bazinga.model.Education;
+import com.bazinga.Bazinga.model.Skill;
+import com.bazinga.Bazinga.repository.EducationRepository;
+import com.bazinga.Bazinga.service.BaseService;
+import com.bazinga.Bazinga.service.EducationService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+/**
+ * Created by lazap on 11/4/2017.
+ */
+public class EducationServiceImpl extends BaseService implements EducationService {
+
+    @Autowired
+    private EducationRepository repository;
+
+    @Override
+    public Education getOne(Long id) {
+        return repository.getOne(id);
+    }
+
+    @Override
+    public List<Education> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Education create(Education education) {
+        return repository.save(education);
+    }
+
+    @Override
+    public Education update(Education education) {
+        return repository.save(education);
+    }
+
+    @Override
+    public void delete(Long id) {
+        repository.delete(id);
+    }
+}
