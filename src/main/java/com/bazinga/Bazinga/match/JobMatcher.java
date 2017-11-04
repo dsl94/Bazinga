96d4dfe0e14d8b586997400d09d0a1811858bd98
nemanja <nemanjabosnjane@gmail.com>
2017-11-04 23:34:06 +0100

    Matching implemetation part 1

--- src/main/java/com/bazinga/Bazinga/match/JobMatcher.java
+++ src/main/java/com/bazinga/Bazinga/match/JobMatcher.java
@@ -1,13 +1,52 @@
 package com.bazinga.Bazinga.match;
 
+import com.bazinga.Bazinga.model.Offer;
+import com.bazinga.Bazinga.model.User;
+import com.bazinga.Bazinga.repository.OfferRepository;
+import com.bazinga.Bazinga.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@EnableScheduling
 public class JobMatcher {
 
     @Autowired
     private List<? extends Matcher> matchers;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private OfferRepository offerRepository;
+
+    @Scheduled(fixedRate = 60000)
+    public void doMatch() {
+        // We have to do matching for every user and for every offer for every user
+        // So first we are starting users
+        List<User> users = userRepository.findAll();
+        List<Offer> offers = offerRepository.findAllByActive(true);
+
+        for (User user : users) {
+            for (Offer offer : offers) {
+                // Now we do match for all matchers
+                if (matchersMatched(user, offer)) {
+                    // Now we check skills
+                }
+            }
+        }
+    }
+
+    private boolean matchersMatched(User user, Offer offer) {
+        for (Matcher matcher : matchers) {
+            if (!matcher.match(user, offer)) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/main/java/com/bazinga/Bazinga/repository/OfferRepository.java
+++ src/main/java/com/bazinga/Bazinga/repository/OfferRepository.java
@@ -3,5 +3,9 @@ package com.bazinga.Bazinga.repository;
 import com.bazinga.Bazinga.model.Offer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OfferRepository extends JpaRepository<Offer, Long> {
+
+    List<Offer> findAllByActive(boolean active);
 }
