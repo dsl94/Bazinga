aa33803f0671ef51f78f0ca4aff4bc26a3f204ba
Lazar Petrov <petrovlazar0@gmail.com>
2017-11-04 15:57:58 +0100

    Added annotation for service

--- src/main/java/com/bazinga/Bazinga/service/impl/EducationServiceImpl.java
+++ src/main/java/com/bazinga/Bazinga/service/impl/EducationServiceImpl.java
@@ -1,17 +1,18 @@
 package com.bazinga.Bazinga.service.impl;
 
 import com.bazinga.Bazinga.model.Education;
-import com.bazinga.Bazinga.model.Skill;
 import com.bazinga.Bazinga.repository.EducationRepository;
 import com.bazinga.Bazinga.service.BaseService;
 import com.bazinga.Bazinga.service.EducationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 /**
  * Created by lazap on 11/4/2017.
  */
+@Service
 public class EducationServiceImpl extends BaseService implements EducationService {
 
     @Autowired
